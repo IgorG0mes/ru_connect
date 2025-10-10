284cfcef038a682ecd76468511c7f759b2e4c17c
igor.gomes <igor.gomes@smartfit.com>
2025-10-09 21:39:32 -0300

    Change attribute name of access logs

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_05_054235) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_10_002552) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   create_table "access_logs", force: :cascade do |t|
     t.integer "status"
     t.datetime "access_time"
-    t.string "denial_reason"
+    t.string "access_result"
     t.string "personable_type", null: false
     t.bigint "personable_id", null: false
     t.datetime "created_at", null: false
