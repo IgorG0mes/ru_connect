e836513f068e75890d2d168441283c00f88c61c9
igor.gomes <igor.gomes@smartfit.com>
2025-10-09 22:12:18 -0300

    create method require_admin for validate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   allow_browser versions: :modern
+
+  protected
+
+  def require_admin
+    unless current_user && current_user.role&.name == "admin"
+      redirect_to root_path, alert: "Acesso negado. Você não tem permissões administrativas para esta ação."
+    end
+  end
 end
